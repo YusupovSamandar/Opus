8d74fd9729d585373db0665ba1c171fc27d928b7
MohinurJalilova <mokhinurjalilova@gmail.com>
2020-11-24 09:52:47 +0500

    GroupLists linked

--- src/component/CardsGroup/CardsGroup.jsx
+++ src/component/CardsGroup/CardsGroup.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import "./CardsGroup.css"
+import "./CardsGroup.css";
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,6 +22,7 @@ export default function Card(props) {
 
     return (
         <Grid item xs={6} sm={3} lg={3} xl={3} md={4} sm={6} >
+            <Link to="/cardsgroup/grouplist">
 
             <Paper id="cardGroup" style={{ borderBottom: "2px solid " + props.color, padding: "20px 10px", marginTop: "10px", boxShadow: "5px 10px 10px #bbbfca" }} className={classes.paper}>
                 <p style={{ fontWeight: "bold" }}>{props.description}</p>
@@ -39,6 +41,7 @@ export default function Card(props) {
                     </Grid>
                 </Grid>
             </Paper>
+            </Link>
         </Grid>
     );
 }
