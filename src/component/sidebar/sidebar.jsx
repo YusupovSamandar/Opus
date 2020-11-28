707a5d5867e8381bad0e1be19d1bb7aa765e6b2e
Kuchendorf <67453378+Kuchendorf@users.noreply.github.com>
2020-11-28 10:26:47 +0500

    commit

--- src/component/sidebar/sidebar.jsx
+++ src/component/sidebar/sidebar.jsx
@@ -19,7 +19,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Language from './lang/Lang';
-import Input from '@    -ui/core/Input';
+import Input from '@material-ui/core/Input';
 import SearchIcon from '@material-ui/icons/Search';
 import { Link } from 'react-router-dom';
 
